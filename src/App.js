262afb56f36284cb84cf85d9a72e5f9eaa5701c1
Andreas Dagnell <andreas.dagnell@gmail.com>
2018-02-02 14:47:52 +0100

    Added styled-components + styled-normalize.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
 
+import styled, { injectGlobal } from 'styled-components'
+import styledNormalize from 'styled-normalize'
+
+const mainBgColor = '#f0f0f0'
+
+injectGlobal`
+  ${styledNormalize}
+
+   body {
+     background-color: ${mainBgColor};
+     font-size: 22px;
+   }
+   * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+   }
+`
+
 class App extends Component {
   render () {
-    return <div>pd</div>
+    return <div className={this.props.className}></div>
   }
 }
 
-export default App
+export default styled(App)`
+  width: 100vw;
+  height: 100vh;
+`
